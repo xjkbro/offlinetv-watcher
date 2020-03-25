55398210c8a25c99bbce90101ac532eb8f46370f
Jason-Kyle De Lara <jason8oh8@gmail.com>
2020-03-24 17:16:03 -0700

    Updates

---
+++ precache-manifest.1d4826a8c0c463137a28f6e3a784924e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41ad6d844567194ab2cbe08d03a85d1",
+    "url": "/offlinetv-watcher/index.html"
+  },
+  {
+    "revision": "ef59ceab63bc7135b8a9",
+    "url": "/offlinetv-watcher/static/css/2.029a8882.chunk.css"
+  },
+  {
+    "revision": "61dc3db28180f2889c51",
+    "url": "/offlinetv-watcher/static/css/main.1141510f.chunk.css"
+  },
+  {
+    "revision": "ef59ceab63bc7135b8a9",
+    "url": "/offlinetv-watcher/static/js/2.71885e1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/offlinetv-watcher/static/js/2.71885e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61dc3db28180f2889c51",
+    "url": "/offlinetv-watcher/static/js/main.bddd860d.chunk.js"
+  },
+  {
+    "revision": "643014a2cf82041fa739",
+    "url": "/offlinetv-watcher/static/js/runtime-main.c43ae9ea.js"
+  },
+  {
+    "revision": "f7b02594f95265d13c80c18efa3bd716",
+    "url": "/offlinetv-watcher/static/media/Offline_TV_Logo.f7b02594.png"
+  },
+  {
+    "revision": "473f5dbe2e7108f3bd7d681ef83d1df0",
+    "url": "/offlinetv-watcher/static/media/instagram.473f5dbe.png"
+  }
+]);
